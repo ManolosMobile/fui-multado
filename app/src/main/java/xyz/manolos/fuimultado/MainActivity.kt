bad79996599b585e90539d95cb2bb17965f1bca4
Phellipe Silva <phellipealexandre@gmail.com>
2019-01-06 15:27:59 -0300

    Migrates dependencies to AndroidX

--- app/src/main/java/xyz/manolos/fuimultado/MainActivity.kt
+++ app/src/main/java/xyz/manolos/fuimultado/MainActivity.kt
@@ -1,6 +1,6 @@
 package xyz.manolos.fuimultado
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
